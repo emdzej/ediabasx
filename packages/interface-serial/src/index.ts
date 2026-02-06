4e0847c32d496abfb3c99a9d886df71a5ae2ecf6
Michał Jaskólski <emdzej@MajBook.lan>
2026-02-06 11:58:14 +0100

    feat(interface-serial): add comm parameter mapping

--- packages/interface-serial/src/index.ts
+++ packages/interface-serial/src/index.ts
@@ -3,10 +3,19 @@ export { MockSerialTransport } from "./mockSerialTransport";
 export { NodeSerialTransport } from "./nodeSerialTransport";
 export { SerialInterface } from "./SerialInterface";
 export * from "./kdcan";
+export {
+  SerialCommParameterIds,
+  SerialInitModes,
+  SerialProtocols
+} from "./types";
 export type {
+  SerialCommParameterId,
+  SerialCommParameterState,
   SerialDataBits,
+  SerialInitMode,
   SerialInterfaceConfig,
   SerialParity,
+  SerialProtocol,
   SerialStopBits,
   SerialTransport,
   SerialTransportConfig
